aa352d55bd086d1d7bccfd84c486d2226af6d948
Carl-Emil Holm Olsen <carlemilholsen@outlook.dk>
2024-01-05 14:22:44 +0100

    Added AdminUsers, Favorites and PriceList in models

---
+++ E-commerce.Logic/Models/Favorites.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Logic.Models
+{
+    public class Favorites
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        //public Users UserId { get; set; }
+        public Products ProductId { get; set; }
+    }
+}
---
+++ E-commerce.Logic/Models/PriceList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Logic.Models
+{
+    public class PriceList
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Products ParentProductId {  get; set; } 
+        public int Price {  get; set; }
+
+    }
+}
