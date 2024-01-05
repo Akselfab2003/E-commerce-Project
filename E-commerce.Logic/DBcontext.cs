321cd268b3361dcab893d4656100c60a5483afd0
Aksel Fabricius <akselfabricius@gmail.com>
2024-01-05 10:40:52 +0100

    Created EF Setup.

---
+++ E-commerce.Logic/DBcontext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_commerce.Logic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce.Logic
+{
+    public class DBcontext : DbContext
+    {
+        public DBcontext(DbContextOptions<DBcontext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Products> Products { get; set; }
+
+
+    }
+}
