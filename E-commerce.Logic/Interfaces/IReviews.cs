42d36f30f73dea35afa910052996ea72ecdc7f53
oliver laursen <olivgaming@gmail.com>
2024-01-05 15:08:14 +0100

    Added the IProducts, IProductVariants, IReviews, ISupportTickets, ITags, IUserDetails og IUsers interfaces and deleted unnecesarry TextFile.

---
+++ E-commerce.Logic/Interfaces/IReviews.cs
@@ -0,0 +1,19 @@
+﻿using E_commerce.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Logic.Interfaces
+{
+    public interface IReviews
+    {
+        public Task<Reviews> Create(Reviews entity);
+        public Task<Reviews> Update(Reviews entity);
+        public Task<bool> Delete(Reviews entity);
+        public Task<Reviews> Get(Reviews entity);
+        public Task<Reviews> GetById(int id);
+        public Task<Reviews> GetByName(string name);
+    }
+}
---
+++ E-commerce.Logic/Interfaces/ITags.cs
@@ -0,0 +1,19 @@
+﻿using E_commerce.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Logic.Interfaces
+{
+    public interface ITags
+    {
+        public Task<Tags> Create(Tags entity);
+        public Task<Tags> Update(Tags entity);
+        public Task<bool> Delete(Tags entity);
+        public Task<Tags> Get(Tags entity);
+        public Task<Tags> GetById(int id);
+        public Task<Tags> GetByName(string name);
+    }
+}
