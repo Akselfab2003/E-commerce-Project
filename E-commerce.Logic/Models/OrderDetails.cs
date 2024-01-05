81342cb7b57e7af4a27329cbd4dc3f79ecaaf588
Aksel Fabricius <akselfabricius@gmail.com>
2024-01-05 14:20:19 +0100

    .

---
+++ E-commerce.Logic/Models/OrderDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Logic.Models
+{
+    public class OrderDetails
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Products ProductId  { get; set; }
+
+        public double price { get; set; }
+
+        public int quantity { get; set; }
+
+        public double total { get; set; }
+
+
+    }
+}
