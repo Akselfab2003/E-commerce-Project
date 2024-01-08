b21ba3f229243aa7f4613f2e704aea44bace4795
Aksel Fabricius <akselfabricius@gmail.com>
2024-01-08 11:48:34 +0100

    Added DependenciesConfiguration.

---
+++ E-commerce Project/DependenciesConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using E_commerce.Logic;
+using E_commerce.Logic.Interfaces;
+using E_commerce.Logic.Models_Logic;
+using E_commerce.Logic.Models_Logic.Table_Repo;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce_Project
+{
+    public static class DependenciesConfiguration
+    {
+        public static IServiceCollection GetConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            
+            services.AddDbContext<DBcontext>(con => con.UseSqlServer(configuration.GetConnectionString("Connection")));
+
+            return services;
+
+
+        }
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IDataCollection, DataCollection>();
+            return services;
+
+        }
+    }
+}
