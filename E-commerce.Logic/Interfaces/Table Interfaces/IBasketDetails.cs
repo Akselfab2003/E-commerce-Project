220363dd776abd28262c853bbdb634388ed8cce0
oliver laursen <olivgaming@gmail.com>
2024-01-08 12:29:45 +0100

    Added Basket, BasketDetails and Session models + migration

---
+++ E-commerce.Logic/Interfaces/Table Interfaces/IBasketDetails.cs
@@ -0,0 +1,20 @@
+﻿using E_commerce.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Logic.Interfaces.Table_Interfaces
+{
+    public interface IBasketDetails
+    {
+        public Task<BasketDetails> GetById(int id);
+
+        public Task<BasketDetails> UpdateBasket(BasketDetails basket);
+
+        public Task<bool> DeleteBasket(int id);
+
+        public Task<BasketDetails> CreateBasket(BasketDetails basket);
+    }
+}
