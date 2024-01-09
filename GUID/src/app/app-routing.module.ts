08c9b33dbd9500d2e6d08469c091a05af816676b
Carl-Emil Holm Olsen <carlemilholsen@outlook.dk>
2024-01-09 13:58:10 +0100

    added some midlertidige products

--- GUID/src/app/app-routing.module.ts
+++ GUID/src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login-page/login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './components/register/register.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
   {path:"Login", component:LoginComponent},
   {path:"Register", component:RegisterComponent},
+  {path:"product-page", component:ProductPageComponent},
+  {path:"home-page", component:HomePageComponent},
 ];
 
 @NgModule({
