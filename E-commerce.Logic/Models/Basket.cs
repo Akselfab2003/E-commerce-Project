f1844759f8a4b557b510c6ed795fb42d27a97911
oliver laursen <olivgaming@gmail.com>
2024-01-09 15:19:16 +0100

    Added the [Key] identifier to the Id property in the Basket model so its explicit instead of implicit (Done to improve clarity).

--- E-commerce.Logic/Models/Basket.cs
+++ E-commerce.Logic/Models/Basket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace E_commerce.Logic.Models
 {
     public class Basket
     {
+        [Key]
         public int Id { get; set; }
         public Users User { get; set; }
         //public Session Session { get; set; }
