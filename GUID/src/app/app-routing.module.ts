1b1db95a67e6fec66777ec0f619dbeade00d1cbf
Carl-Emil Holm Olsen <carlemilholsen@outlook.dk>
2024-01-09 12:39:25 +0100

    .

--- GUID/src/app/app-routing.module.ts
+++ GUID/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login-page/login-page.component';
+import { LoginComponent } from './components/login-page/login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {path:"Login", component:LoginComponent},
--- GUID/src/app/app.module.ts
+++ GUID/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login-page/login-page.component';
+import { LoginComponent } from './components/login-page/login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { ProductPageListComponent } from './product-page-list/product-page-list.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
+import { ProductPageListComponent } from './components/product-page-list/product-page-list.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 
 @NgModule({
