7768d910c1abbd09369ed3f91f7df6d1f0f87388
Aksel Fabricius <akselfabricius@gmail.com>
2024-01-09 12:00:03 +0100

    tried to add order for unit tests

--- E-commerce.Test/UnitTest1.cs
+++ E-commerce.Test/UnitTest1.cs
@@ -1,37 +1,67 @@
 using E_commerce.Logic.Interfaces;
 using E_commerce.Logic.Models;
+using System.Text.Json;
+using Xunit.Abstractions;
 
 namespace E_commerce.Test
 {
-    [Collection("Services")]
+   // [TestCaseOrderer("E_commerce.Test.OrderedTest", "E_commerce.Test")]
 
+    [Collection("Services")]
     public class UnitTest1
     {
         private readonly IDataCollection dataCollection;
 
-        public UnitTest1(CreateFakeDBDependencies collection)
+        private readonly ITestOutputHelper output;
+        public UnitTest1(CreateFakeDBDependencies collection,ITestOutputHelper outputHelper)
         {
             dataCollection = collection.DataCollection;
+            output = outputHelper;
         }
 
         [Fact]
-        public async void Test1()
+        public async void TestA()
         {
-           Orders orders = new Orders();
-           orders.Id = 1;
-           orders.sessid = "Test";
-           Orders orderReturned = await dataCollection.Orders.CreateOrder(orders);
-           
-           Assert.Equal(orders, orderReturned);
+        
+                Orders orders = new Orders();
+                orders.Id = 1;
+                 orders.sessid = "Test";
+                Orders orderReturned = await dataCollection.Orders.CreateOrder(orders);
+                output.WriteLine(JsonSerializer.Serialize(orderReturned));
+                Assert.Equal(orders, orderReturned);
+        
+
+
+        }
+        [Fact]
+        public async void TestD()
+        {
+            Orders order = await dataCollection.Orders.GetById(1);
+
+            output.WriteLine(JsonSerializer.Serialize(order));
+
+            Assert.Null(order);
+        }
+
+        [Fact]
+        public async void TestB()
+        {
+        
+            await dataCollection.Orders.DeleteOrder(1);
+            Orders order = await dataCollection.Orders.GetById(1);
+            output.WriteLine(JsonSerializer.Serialize(order));
+               
           
+               
+
+            Assert.Null(order);
+
+
         }
 
-        [Fact]
-        public void Test2()
-        {
-            bool result = 1 == 1;
 
-            Assert.True(result);
-        }
+
+
+    
     }
 }
