6eb7c884d2529fdfd913abc36b43765219ac28f6
Carl-Emil Holm Olsen <carlemilholsen@outlook.dk>
2024-01-10 14:28:53 +0100

    added routing to product card

--- GUID/src/app/app-routing.module.ts
+++ GUID/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { ProductPageComponent } from './components/product-page/product-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProductSiteComponent } from './components/product-site/product-site.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 const routes: Routes = [
   {path:"",component:HomePageComponent},
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path:"Register", component:RegisterComponent},
   {path:"product-page", component:ProductPageComponent},
   {path:"home-page", component:HomePageComponent},
-  {path:"product-Site/:id", component:ProductSiteComponent}
+  {path:"product-Site/:id", component:ProductSiteComponent},
+  {path: "product-card", component:ProductCardComponent}
 ];
 
 @NgModule({
