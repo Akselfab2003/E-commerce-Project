07535fda05a9f2697e26d48924e0a456c7f111d9
Emil Isak Lundqvist <emil372r@elev.tec.dk>
2024-01-11 10:30:03 +0100

    .

--- GUID/src/app/components/login-page/login-page.component.ts
+++ GUID/src/app/components/login-page/login-page.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, NgModule} from '@angular/core';
 import { FormControl, FormGroup, Validators, AbstractControl, ValidatorFn, ValidationErrors, ReactiveFormsModule} from '@angular/forms';
 import { User } from '../../models/User';
+import { HttpserviceService } from '../../../Services/httpservice.service';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-login',
   templateUrl: './Login-page.component.html',
   styleUrls: ['./Login-page.component.css']
 })
-export class LoginComponent {
+export class LoginComponent<T> {
   title = "Login-page";
   //Gemmer brugerens input i variabler
   loginForm = new FormGroup({
@@ -15,16 +17,12 @@ export class LoginComponent {
     password: new FormControl('', Validators.required),
   });
 
-  /*constructor(){
-    this.loginForm.valueChanges.subscribe((value)=>{
-      console.log(value);
-    });
-  }*/
+  
+
 
   //starter forfra hvis login ikke passer
   login(){
     if(this.loginForm.invalid) return;
-
     console.log('calling backend to login');
   }
 }
