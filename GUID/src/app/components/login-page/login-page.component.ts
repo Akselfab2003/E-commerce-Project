64fb5a7e34a1d8ecf4a918359670c1e3277297f2
Emil Isak Lundqvist <emil372r@elev.tec.dk>
2024-01-11 15:23:02 +0100

    .

--- GUID/src/app/components/login-page/login-page.component.ts
+++ GUID/src/app/components/login-page/login-page.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent<T> {
   login(){
     let username:string = this.loginForm.get("username")?.value?.toString() as string;
     let password:string = this.loginForm.get("password")?.value?.toString() as string;
-    this.service.GetRequest<User>("User/"+password+username).subscribe((data)=>
+    this.service.GetRequest<User>("User/").subscribe((data)=>
     console.log(data)
     )
     if(this.loginForm.invalid) return;
