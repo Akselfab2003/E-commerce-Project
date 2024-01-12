4b7e7f98334c46a9b744f6a5eca0520235f15e84
Aksel Fabricius <akselfabricius@gmail.com>
2024-01-12 13:41:57 +0100

    .

---
+++ E-commerce.Test/Create data for local database/FillDatabaseWithData.cs
@@ -0,0 +1,55 @@
+﻿using Bogus;
+using E_commerce.Logic;
+using E_commerce.Logic.Interfaces;
+using E_commerce.Logic.Models;
+using E_commerce.Logic.Models_Logic;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace E_commerce.Test.Create_data_for_local_database
+{
+    [Collection("LocalDatabaseService")]
+    public class FillDatabaseWithData
+    {
+        private readonly ITestOutputHelper output;
+        private  IDataCollection DataCollection;
+
+        public FillDatabaseWithData(GenerateFakeDataForDatabase collection,ITestOutputHelper outputHelper) 
+        {
+            this.output = outputHelper;
+            this.DataCollection = collection.DataCollection;
+        
+        }
+
+        [Fact]
+        public async void InsertDataToDatabase()
+        {
+            Faker<Users> faker = new Faker<Users>()
+                //.RuleFor(user => user.Id, data => data.IndexFaker)
+                .RuleFor(user => user.Username, data => data.Person.UserName)
+                .RuleFor(user => user.Password, data => data.Internet.Password(10))
+                .RuleFor(user => user.Email, data => data.Person.Email)
+                .RuleFor(user => user.Gender, data => data.Random.Bool());
+
+
+            List<Users> data = faker.GenerateBetween(10, 20);
+
+            output.WriteLine(JsonSerializer.Serialize(data));
+            foreach (Users user in data)
+            {
+                await DataCollection.Users.CreateUser(user);
+            }
+            Assert.True(data.Any());
+            
+        }
+    }
+}
---
+++ E-commerce.Test/Create data for local database/GenerateFakeDataForDatabase.cs
@@ -0,0 +1,44 @@
+﻿using E_commerce.Logic;
+using E_commerce.Logic.Interfaces;
+using E_commerce.Logic.Models_Logic;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace E_commerce.Test
+{
+    public class GenerateFakeDataForDatabase
+    {
+        private readonly DBcontext Context;
+        private readonly IDataCollection dataCollection;
+        public GenerateFakeDataForDatabase()
+        {
+            
+            SecretClass test = JsonSerializer.Deserialize<SecretClass>(File.ReadAllText("..\\net8.0\\Create data for local database\\Secret.json"));
+            var connection = new SqlConnection(test.Connection);
+            connection.Open();
+            DbContextOptions<DBcontext> contextOptions =
+            new DbContextOptionsBuilder<DBcontext>().UseSqlServer(connection).Options;
+            
+            Context = new DBcontext(contextOptions);
+            dataCollection = new DataCollection(Context);
+
+        }
+        public IDataCollection DataCollection
+        {
+            get { return dataCollection; }
+        }
+
+        public class SecretClass
+        {
+            public string Connection { get; set; }
+        }
+    }
+}
