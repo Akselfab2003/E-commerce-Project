7159a62fbf9d70c8d6b457ba02b63a39e68c64aa
Carl-Emil Holm Olsen <carlemilholsen@outlook.dk>
2024-01-12 13:47:15 +0100

    Added User page

--- GUID/src/app/components/profile/profile.component.ts
+++ GUID/src/app/components/profile/profile.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/User';
+import { HttpserviceService } from '../../../Services/httpservice.service';
+import { Order } from '../../models/Order';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent<T> {
 
+  user:User = new User();
+  order:Order = new Order();
+
+  constructor(private service:HttpserviceService<T>) { };
+
+
+  ngOnInit(): void {
+    this.GetUser();
+    this.GetOrders();
+  };
+
+  GetUser(){
+    this.service.GetRequest<User>("User/test").subscribe((data)=>{
+      this.user = data;
+      console.log(this.user)
+    });
+  }
+
+  GetOrders(){
+    this.service.GetRequest<Order>("api/Orders").subscribe((data)=>{
+      this.order = data;
+      console.log(this.order)
+    });
+  }
 }
