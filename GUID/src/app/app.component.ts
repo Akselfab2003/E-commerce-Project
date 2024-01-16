108cd16ffd553cbb4d3bb81a52df8ed7ae5a49b0
Aksel Fabricius <akselfabricius@gmail.com>
2024-01-16 10:05:33 +0100

    fix to sessionlogic.

--- GUID/src/app/app.component.ts
+++ GUID/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpserviceService } from '../Services/httpservice.service';
+import { sessionController } from './logic/sessionLogic';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent<T> {
   title = 'GUID';
+  constructor(private service:HttpserviceService<T>){
+    this.CheckifSessionShouldBeCreated()
+  }
+
+  CheckifSessionShouldBeCreated(){
+    console.log("Test If This Runs As The First method")
+    var CurrentCookieValue:any = sessionController.GetCookie()
+    if (CurrentCookieValue==undefined || CurrentCookieValue == null ){
+        sessionController.CreateEmptySession(this.service)
+    }
+    return true;
+  }
+
+
 }
