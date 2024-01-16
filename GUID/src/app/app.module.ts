ca4a6236379f51575ef4b1b28bc93b5c2609cfb5
Carl-Emil Holm Olsen <carlemilholsen@outlook.dk>
2024-01-16 10:53:41 +0100

    Added quick AdminPage

--- GUID/src/app/app.module.ts
+++ GUID/src/app/app.module.ts
@@ -15,8 +15,9 @@ import { ProductCardComponent } from './components/product-card/product-card.com
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProductDetailsPageComponent } from './components/product-details-page/product-details-page.component';
 import { BasketComponent } from './components/basket/basket.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {CheckoutPageComponent } from './components/checkout-page/checkout-page.component';
+import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { CheckoutPageComponent } from './components/checkout-page/checkout-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 
 @NgModule({
@@ -33,7 +34,8 @@ import {CheckoutPageComponent } from './components/checkout-page/checkout-page.c
     ProfileComponent,
     ProductDetailsPageComponent,
     BasketComponent,
-    CheckoutPageComponent
+    CheckoutPageComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule,
